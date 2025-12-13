97731e68301969d865b521e08bf929a1a8be01ac
Fumihito TSUJIKAWA <readme627@gmail.com>
2025-12-14 00:10:11 +0900

    Buttonコンポーネントを追加し、クリックイベントとリンク機能を実装

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,46 @@
-import "./App.css";
+const Button = ({ onClick, href, children }) => {
+  const style = {
+    backgroundColor: "skyblue",
+    border: "none",
+    padding: "8px",
+    margin: "8px",
+    borderRadius: "8px",
+    color: "white",
+    cursor: "pointer",
+    lineHeght: 1,
+  };
 
-function App() {
-  return <h1>JSXの基本</h1>;
-}
+  if (onClick) {
+    return (
+        <button type="button" onClick={onClick}
+        style={style}>
+        {children}
+        </button>
+    );
+  }
+
+  if (href) {
+    return (
+        <a href={href} style={style}>
+            {children}
+        </a>
+    );
+  }
+};
+
+const App = () => {
+    const handleClick = () => {
+        alert("button要素がクリックされました");
+    };
+
+    const href = "https://ja.react.dev/";
+
+    return (
+        <>
+        <Button onClick={handleClick}>ボタン</Button>
+        <Button href={href}>リンク</Button>
+        </>
+    );
+};
 
 export default App;
