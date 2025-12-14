000494aa2425a20b964cf9340a16e3f2e55c482a
Fumihito TSUJIKAWA <readme627@gmail.com>
2025-12-14 17:55:17 +0900

    Counterコンポーネントを実装し、クリック回数をカウントする機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,19 @@
-const Button = ({ onClick, href, children }) => {
-  const style = {
-    backgroundColor: "skyblue",
-    border: "none",
-    padding: "8px",
-    margin: "8px",
-    borderRadius: "8px",
-    color: "white",
-    cursor: "pointer",
-    lineHeght: 1,
+import { useState } from 'react';
+
+const Counter = () => {
+  const [count, setCount] = useState(0);
+
+  const handleClick = () => {
+    setCount(prevCount => prevCount + 1);
   };
 
-  if (onClick) {
-    return (
-        <button type="button" onClick={onClick}
-        style={style}>
-        {children}
-        </button>
-    );
-  }
+  return (
+    <div>
+      <button onClick={handleClick}>
+        {count} 回押しました
+      </button>
+    </div>
+  );
+}
 
-  if (href) {
-    return (
-        <a href={href} style={style}>
-            {children}
-        </a>
-    );
-  }
-};
-
-const App = () => {
-    const handleClick = () => {
-        alert("button要素がクリックされました");
-    };
-
-    const href = "https://ja.react.dev/";
-
-    return (
-        <>
-        <Button onClick={handleClick}>ボタン</Button>
-        <Button href={href}>リンク</Button>
-        </>
-    );
-};
-
-export default App;
+export default Counter;
