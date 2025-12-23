8c7197e079a7d612d74272234bf8c029fd8ff059
Fumihito TSUJIKAWA <readme627@gmail.com>
2025-12-23 18:48:55 +0900

    useReducerを使用したタスク管理アプリを実装し、タスクの作成と削除機能を追加

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,54 @@
-const Button = ({ onClick, href, children }) => {
-  const style = {
-    backgroundColor: "skyblue",
-    border: "none",
-    padding: "8px",
-    margin: "8px",
-    borderRadius: "8px",
-    color: "white",
-    cursor: "pointer",
-    lineHeght: 1,
+import { useReducer, useState } from "react";
+
+const initialState = [
+  { id: Date.now(), title: "ダンブルタスク" },
+];
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "CREATE":
+      return [
+        ...state,
+        { id: Date.now(), title: action.payload },
+      ];
+    case "DELETE":
+      return state.filter(
+        (task) => task.id !== action.payload,
+      );
+    default:
+      return state;
+  }
+};
+
+const TaskApp = () => {
+  const [tasks, dispatch] = useReducer(reducer, initialState);
+  const [inputText, setInputText] = useState("");
+
+  const handleTaskCreate = () => {
+    dispatch({ type: "CREATE", payload: inputText });
+    setInputText("");
   };
 
-  if (onClick) {
-    return (
-        <button type="button" onClick={onClick}
-        style={style}>
-        {children}
-        </button>
-    );
-  }
+  const handleTaskDelete = (id) => {
+    dispatch({ type: "DELETE", payload: id });
+  };
 
-  if (href) {
-    return (
-        <a href={href} style={style}>
-            {children}
-        </a>
-    );
-  }
+  return (
+    <div>
+      <input type = "text" value={inputText}
+      onChange={(e) => setInputText(e.target.value)}
+      placeholder="タスクタイトルを入力"/>
+      <button onClick={handleTaskCreate}>作成</button>
+      <ul>
+        {tasks.map((task) => (
+          <li key={task.id}>
+            {task.title}
+            <button onClick={() => handleTaskDelete(task.id)}>削除</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  )
 };
 
-const App = () => {
-    const handleClick = () => {
-        alert("button要素がクリックされました");
-    };
-
-    const href = "https://ja.react.dev/";
-
-    return (
-        <>
-        <Button onClick={handleClick}>ボタン</Button>
-        <Button href={href}>リンク</Button>
-        </>
-    );
-};
-
-export default App;
+export default TaskApp;
